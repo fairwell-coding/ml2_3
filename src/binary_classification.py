1a9ba319dbb32661ef111037c1711eb55f290811
Ingmar <ingmar.lercher@hotmail.com>
2022-06-06 15:43:57 +0200

    task1 & task2 (E is missing)

--- src/binary_classification.py
+++ src/binary_classification.py
@@ -28,8 +28,8 @@ def load_data():
     """ Start of your code 
     """
 
-    data_a_train, data_a_test = np.load('data_a_test.npy'), np.load('data_a_test.npy')
-    data_b_train, data_b_test = np.load('data_b_train.npy'), np.load('data_b_test.npy')
+    data_a_train, data_a_test = np.load('src/data_a_test.npy'), np.load('src/data_a_test.npy')
+    data_b_train, data_b_test = np.load('src/data_b_train.npy'), np.load('src/data_b_test.npy')
 
     """ End of your code
     """
@@ -71,9 +71,22 @@ def quadratic():
     Y_test = data_a_test[:, 2] # Y = 100x1
     PHI =  feature_transform(X_test)
     Y_test_pred = np.sign(PHI @ w.T)
+ 
+    train_acc, test_acc = len(np.nonzero(Y_train == Y_train_pred)[0]) / N, len(np.nonzero(Y_test == Y_test_pred)[0]) / N
 
-    train_error, test_error = len(np.nonzero(Y_train == Y_train_pred)[0]) / N, len(np.nonzero(Y_test == Y_test_pred)[0]) / N
+    C1_ind = np.nonzero(Y_test >= 0)
+    C2_ind = np.nonzero(Y_test < 0)
+    marker_size = 60
+    ax[0].scatter(X_test[C1_ind, 0], X_test[C1_ind, 1], marker_size, c='blue', marker='o', label='test data c1 (+)')
+    ax[0].scatter(X_test[C2_ind, 0], X_test[C2_ind, 1], marker_size, c='red',  marker='o', label='test data c2 (x)')
 
+    test_indices_C1 = np.nonzero(Y_test_pred >= 0)
+    test_indices_C2 = np.nonzero(Y_test_pred < 0)
+    ax[1].scatter(X_test[C1_ind, 0], X_test[C1_ind, 1], marker_size, c='blue', marker='o', label='test data c1 (+)')
+    ax[1].scatter(X_test[C2_ind, 0], X_test[C2_ind, 1], marker_size, c='red',  marker='o', label='test data c2 (x)')
+    
+    ax[1].scatter(X_test[test_indices_C1, 0], X_test[test_indices_C1, 1], marker_size, c='k', marker='+', label='test prediction c1')
+    ax[1].scatter(X_test[test_indices_C2, 0], X_test[test_indices_C2, 1], marker_size, c='k',  marker='x', label='test prediction c2')
 
     """ End of your code
     """
@@ -83,11 +96,9 @@ def quadratic():
     plt.show()
     return fig
 
-
 def feature_transform(X):
     N = X.shape[0]
-    return np.stack([np.ones((N,)), X[:, 0], X[:, 1]]).T  # PHI = 100x3
-
+    return np.stack([np.ones((N,)), X[:, 0], X[:, 1]**2]).T # PHI = 100x3
 
 # def __calculate_w(theta, Y):
 #     A = theta.T @ theta
@@ -96,11 +107,9 @@ def feature_transform(X):
 #     w = np.linalg.solve(R, z)
 #     return w
 
-
 def invert(A):
     Q, R = np.linalg.qr(A)
     return np.linalg.inv(R) @ Q.T
-
 
 def logistic():
     """ Subtask 2: Logistic Loss as Convex Surrogate in Binary Classification
@@ -133,27 +142,35 @@ def logistic():
     k_max = 1000
     #TODO: calculate engery
     E = np.zeros((k_max, 1))
-    E_app = np.zeros((k_max, 1))
-    w, E, E_app = nesterov_gradient(Y_train, PHI, w0, L, k_max)
-    Y_train_pred = np.sign(sigmoid(PHI @ w) - 0.5)
+    w, E = nesterov_gradient(Y_train, X_train, w0, L, k_max)
+    # Y_train_pred = np.sign(sigmoid(PHI @ w) - 0.5)
+    Y_train_pred = np.where(sigmoid(PHI @ w) < 0.5, -1 ,1)
 
     X_test = np.stack([data_a_test[:, 0], data_a_test[:, 1]]).T # X = 100x2
     Y_test = data_a_test[:, 2] # Y = 100x1
 
     PHI = feature_transform(X_test)
-    L = calculate_lipschitz(PHI)
-    w0 = np.random.uniform(size=(M, 1))
-    Y_test_pred = np.sign(sigmoid(PHI @ w) - 0.5)
+    Y_test_pred = np.where(sigmoid(PHI @ w) < 0.5, -1 ,1)
 
 
-    ax[0].plot(np.arange(1, k_max+1), E)
+    iters = np.arange(1, len(E))
+    ax[0].loglog(iters, E[:-1]-E.min(), color="blue", linewidth=2.0, label="Logistic loss")
+    # ax[0].loglog(iters, E.max()/iters**2, color="black", linewidth=2.0, label="$O(1/k^2)$")
 
-    marker_size = 30
-    ax[1].scatter(X_train[:, 0], X_train[:, 1], marker_size*Y_train, c='blue', marker='o', label='train data')
-    ax[1].scatter(X_train[:, 0], X_train[:, 1], marker_size*Y_train_pred, c='red',  marker='x', label='train prediction')
+    marker_size = 60
 
-    ax[2].scatter(X_test[:, 0], X_test[:, 1], marker_size*Y_test, c='k', marker='o', label='test data')
-    ax[2].scatter(X_test[:, 0], X_test[:, 1], marker_size*Y_test_pred, c='orange', marker='x', label='test prediction')
+    C1_ind = np.nonzero(Y_test == 1)
+    C2_ind = np.nonzero(Y_test == -1)
+    ax[1].scatter(X_test[C1_ind, 0], X_test[C1_ind, 1], marker_size, c='blue', marker='o', label='test data c1 (+)')
+    ax[1].scatter(X_test[C2_ind, 0], X_test[C2_ind, 1], marker_size, c='red',  marker='o', label='test data c2 (x)')
+
+    test_indices_C1 = np.nonzero(Y_test_pred == 1)
+    test_indices_C2 = np.nonzero(Y_test_pred == -1)
+    ax[2].scatter(X_test[C1_ind, 0], X_test[C1_ind, 1], marker_size, c='blue', marker='o', label='test data c1 (+)')
+    ax[2].scatter(X_test[C2_ind, 0], X_test[C2_ind, 1], marker_size, c='red',  marker='o', label='test data c2 (x)')
+
+    ax[2].scatter(X_test[test_indices_C1, 0], X_test[test_indices_C1, 1], marker_size, c='k', marker='+', label='test prediction c1')
+    ax[2].scatter(X_test[test_indices_C2, 0], X_test[test_indices_C2, 1], marker_size, c='k',  marker='x', label='test prediction c2')
 
     """ End of your code
     """
@@ -166,24 +183,41 @@ def logistic():
 def calculate_lipschitz(PHI):
     return 1/4 * np.amax(sigmoid(PHI)) #largest singular value
 
-def nesterov_gradient(y, PHI, w_k, L, k_max):
 
-    E_ = np.zeros((k_max, 1))
-    E_app = np.zeros((k_max, 1))
-    E_prime = lambda w_: sigmoid(y @ PHI @ w_) - 1
-    E = lambda w_: -np.log(sigmoid(y @ PHI @ w_))
-    w_prev = w_k
+def log_loss(X, y, w):
+    N = len(y)
+    PHI = X#feature_transform(X.reshape((N, 2)))
+    w = w.reshape((3,1))
+    y = y.reshape((N, 1))
+    return np.sum(np.log(1 + np.exp(-y * PHI @ w)), axis=0)
+    # return -np.sum(np.log(sigmoid(y * PHI @ w)))
+
+def grad_log_loss(X, y, w):
+    N = len(y)
+    PHI = X# feature_transform(X.reshape((N, 2)))
+    w = w.reshape((3,1))
+    y = y.reshape((N, 1))
+    return np.sum(PHI.T @ (y * (sigmoid(y * PHI @ w) - 1)), axis=1)
+
+def nesterov_gradient(y, X, w0, L, k_max):
+
+    PHI = feature_transform(X)
+    y = y.reshape((100, 1))
+    E_ = []
+    E_app = np.zeros((3,1))
+    w_old = w0
+    w = w0
     for k in range(1, k_max+1):
         beta = (k-1)/(k+1)
-        w_k = w_k + beta * (w_k - w_prev)
-        w_prev = w_k
+        w_ = w  + beta * (w - w_old)
+        E_.append(log_loss(PHI, y, w_))
+        if (k-1)%10 == 0:      
+            E_app = approx_fprime(w_.flatten(), lambda w__: log_loss(PHI, y, w__), 1e-8)
+            print(f'{log_loss(PHI, y, w_)} | {E_app.flatten()} | {grad_log_loss(PHI, y, w_).flatten()}')
+        w_old = w
+        w = w_ - 1/L * grad_log_loss(PHI, y, w_).reshape((3, 1))
 
-        #TODO: calculate energy (E), not correct yet
-        E_[k-1] = E_prime(w_k)
-        # E_app[k-1] = approx_fprime(w_k, E)
-        w_k = w_k - 1/L * E_prime(w_k)
-
-    return w_k, E_, E_app
+    return w, np.array(E_)
 
 def svm_primal():
     """ Subtask 3: Hinge Loss as Convex Surrogate in Binary Classification
@@ -213,8 +247,8 @@ def svm_primal():
     y_test = data_a_test[:, 2]  # y = 100x1
 
     # hyperparameters
-    lambda_ = 1.24  # 1.24
-    alpha = 1e-2
+    lambda_ = 1e-4  # 1.24
+    alpha = 0.2
     w = np.ones((3, 1))
     w[0] = 0.0
     delta = 1e-4
@@ -222,14 +256,14 @@ def svm_primal():
     PHI = feature_transform(X_train)
     w, b = __proximal_subgradient_method(PHI, y_train, alpha, lambda_, w, delta)
 
-    y_train_pred = np.sign(feature_transform(X_train) @ w)
-    y_test_pred = np.sign(feature_transform(X_test) @ w)
+    y_train_pred = np.sign(feature_transform(X_train) @ w + b)
+    y_test_pred = np.sign(feature_transform(X_test) @ w + b)
 
     train_acc, test_acc = len(np.nonzero(y_train.reshape(y_train.shape[0], 1) == y_train_pred)[0]) / X_train.shape[0], len(np.nonzero(y_test.reshape(y_test.shape[0], 1) == y_test_pred)[0]) / \
                           X_test.shape[0]
     print(f'SVM primal space: train_acc = {train_acc}, test_acc = {test_acc}')
 
-    __plots_svm_primal(PHI, X_train, ax, w)
+    __plots_svm_primal(PHI, X_train, ax, w, b)
 
     """ End of your code
     """
@@ -239,29 +273,29 @@ def svm_primal():
     return fig
 
 
-def __plots_svm_primal(PHI, X_train, ax, w):
+def __plots_svm_primal(PHI, X_train, ax, w, b):
     marker_size = 30
     ax[0].scatter(X_train[:50, 0], X_train[:50, 1], marker_size, c='blue', marker='o', label='train data')  # class 1
     ax[0].scatter(X_train[50:, 0], X_train[50:, 1], marker_size, c='red', marker='o', label='train data')  # class -1
-    plot_decision_boundary(X_train, w, ax[0], plot_step=0.01)
+    plot_decision_boundary(X_train, w, ax[0], b, plot_step=0.01)
     plt.show()
 
 
-def plot_decision_boundary(X, w, ax, plot_step=0.05):
+def plot_decision_boundary(X, w, ax, b, plot_step=0.05):
     x_min, x_max = X[:, 0].min(), X[:, 0].max()
     y_min, y_max = X[:, 1].min(), X[:, 1].max()
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                            np.arange(y_min, y_max, plot_step))
 
-    Z = feature_transform(np.c_[xx.ravel(), yy.ravel()]) @ w
+    Z = feature_transform(np.c_[xx.ravel(), yy.ravel()]) @ w + b
     Z = Z.reshape(xx.shape)
-    hyperplane = np.where(np.round(Z, 3) == 0, 1, 0)
+    hyperplane = np.where(np.round(Z, 2) == 0, 1, 0)
     ax.contour(xx, yy, hyperplane, colors='black')
 
-    class1 = np.where(np.round(Z, 3) == 1, 1, 0)
+    class1 = np.where(np.round(Z, 2) == 1, 1, 0)
     ax.contour(xx, yy, class1, colors='grey')
 
-    class2 = np.where(np.round(Z, 3) == -1, 1, 0)
+    class2 = np.where(np.round(Z, 2) == -1, 1, 0)
     ax.contour(xx, yy, class2, colors='grey')
 
 
@@ -272,29 +306,30 @@ def __proximal_subgradient_method(PHI, y, alpha, lambda_, w_i_1, delta):
     b = w_i_1[0]
 
     while (42):
-        # b = b - alpha
-        # w_i_1[0] = w_i_1[0] - alpha
-        # b = 1 - y.T @ PHI @ w_i_1  # update bias
         b = np.mean(y.reshape(100, 1) - (PHI @ w_i_1))
         w_schlange = np.concatenate((b.reshape(1, 1), w_i_1[1:]))
         # g = np.where(y.T @ (PHI @ w_schlange) >= 1, np.zeros((3, )), - PHI.T @ y).reshape(3, 1) * 1 / N  # analytical_grad = g
         g = np.mean(np.where(y.reshape((100, 1)) * (PHI @ w_i_1 + b) >= 1, 0, -y.reshape((100, 1)) * PHI), axis=0).reshape(3, 1)
-        w_i_1_schlange = w_schlange - alpha * g
-        w_i_1 = w_i_1 / (1 + lambda_ * alpha)
-
-        # compare numerical and analytical gradients
         numerical_grad = 0
         for i in range(y.shape[0]):
             numerical_grad += approx_fprime(PHI[i], __hinge_loss, 1e-8, y[i], lambda_, w_schlange)
 
         numerical_grad = numerical_grad / N
+        # w_i_1_schlange = w_schlange - alpha * g
+        # w_i_1 = w_i_1 / (1 + lambda_ * alpha)
+        
+        w_i_1_schlange = w_schlange - alpha * numerical_grad.reshape(3, 1)
+        w_i_1 = w_i_1 / (1 + lambda_ * alpha)
 
-        grad_diff = np.abs(g.flatten() - numerical_grad)
+        # compare numerical and analytical gradients
 
-        w_diff = np.abs(w_i_1_schlange - w_i_1_schlange_old)
+
+        grad_diff = np.abs(g.flatten() - numerical_grad).flatten()
+
+        w_diff = np.abs(w_i_1_schlange - w_i_1_schlange_old).flatten()
         print(f'Gradient, w and b diff for iteration {epoch} = {grad_diff} | {w_diff} | {b}.')
 
-        if epoch > 1 and np.all(w_diff < delta):
+        if epoch > 1000 or np.all(w_diff < 1e-8):
             return w_i_1_schlange, b
 
         w_i_1_schlange_old = w_i_1_schlange
@@ -354,10 +389,11 @@ if __name__ == '__main__':
     sigmoid = (lambda x: 1/(1+np.exp(-x)))
 
     # tasks = [quadratic, logistic, svm_primal, svm_dual]
-    tasks = [svm_primal]
+    tasks = [logistic]
     pdf = PdfPages('figures.pdf')
     for task in tasks:
         f = task()
         pdf.savefig(f)
 
     pdf.close()
+
